619d2e7fafb788b06a65a31b7854bdd131e37cd5
Henrique Rojas <henriquerojinhas@gmail.com>
2023-08-28 15:41:06 -0300

    Código atualizado: função para cálculo de erro

--- Lista 2 - Métodos numéricos/lagranje.py
+++ Lista 2 - Métodos numéricos/lagranje.py
@@ -1,3 +1,6 @@
+import math
+
+
 def lagrange_interpolation(x_values, y_values, x):
     n = len(x_values)
     result = 0.0
@@ -16,3 +19,40 @@ valores_y = [0.043, 1.928, 2.497, 3.875, 9.000, 13.467, 19.176]
 ponto = 1.4
 
 print(lagrange_interpolation(valores_x, valores_y, ponto))
+
+
+def interpolation_error(x, x_values, y_values):
+    """
+    Calcula o erro de interpolação em um ponto x específico.
+    
+    x: O ponto no qual se deseja calcular o erro de interpolação.
+    x_values: Lista dos valores conhecidos de x.
+    y_values: Lista dos valores conhecidos de y correspondentes aos valores de x.
+    
+    Retorna o erro de interpolação em x.
+    """
+    n = len(x_values)
+    max_derivative = 1  # Ordem máxima da derivada das funções interpolantes
+    
+    error = 0.0
+    for i in range(n):
+        term = 1.0
+        for j in range(n):
+            if j != i:
+                term *= abs(x - x_values[j]) / abs(x_values[i] - x_values[j])
+        error += term
+    
+    error *= max_derivative * max(y_values) / (math.factorial(n) * (2**n))
+    
+    return error
+
+# Exemplo de uso
+x_values = [0, 1, 2, 3, 4]
+y_values = [0, 1, 4, 9, 16]
+interpolation_point = 2.5
+
+interpolated_value = lagrange_interpolation(x_values, y_values, interpolation_point)
+error = interpolation_error(interpolation_point, x_values, y_values)
+
+print(f"Valor interpolado em x = {interpolation_point}: {interpolated_value}")
+print(f"Erro de interpolação em x = {interpolation_point}: {error}")
