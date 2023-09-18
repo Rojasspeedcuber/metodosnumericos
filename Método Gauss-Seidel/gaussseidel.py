ac4b3dec2dbe27070202f2aebedbfa9931500844
Henrique Rojas <henriquerojinhas@gmail.com>
2023-09-18 14:36:32 -0300

    Método Gauss Seidel testado!!

--- Método Gauss-Seidel/gaussseidel.py
+++ Método Gauss-Seidel/gaussseidel.py
@@ -1,34 +1,44 @@
-def gauss_seidel(A, b, tol=10e-7, max_iter=100):
-    n = len(b)
-    x = [0.0] * n
-    iter_count = 0
+import numpy as np
 
+def gauss_seidel(A, b, x0, tol=10e-7, max_iter=100):
+    n = len(b)
+    x = x0.copy()
+    iters = 0
+    
     for _ in range(max_iter):
         x_new = x.copy()
         for i in range(n):
-            sigma = sum(A[i][j] * x_new[j] for j in range(n) if j != i)
-            x_new[i] = (b[i] - sigma) / A[i][i]
-
-        iter_count += 1
-
-        # Verifica a convergência com base na tolerância
-        if all(abs(x_new[i] - x[i]) < tol for i in range(n)):
+            s1 = np.dot(A[i, :i], x_new[:i])
+            s2 = np.dot(A[i, i+1:], x[i+1:])
+            x_new[i] = (b[i] - s1 - s2) / A[i, i]
+        
+        residual = np.abs(b - np.dot(A, x_new))
+        
+        if np.all(residual < tol):
             break
-
+        
         x = x_new
+        iters += 1
+    
+    return x, iters
 
-    return x, iter_count
-
-# Exemplo de uso
-A = [[4, -1, 0, -1, 0, 0, 0, 0, 0, 0],
+# Exemplo de uso:
+A = np.array([[4, -1, 0, -1, 0, 0, 0, 0, 0, 0],
      [-1, 4, -1, 0, -1, 0, 0, 0, 0, 0],
      [0, -1, 4, 0, 0, -1, 0, 0, 0, 0],
      [-1, 0, 0, 4, -1, 0, 0, 0, 0, 0],
      [0, -1, 0, -1, 4, -1, -1, 0, 0, 0],
-     [0, -1, 4, 0, 0, -1, 0, 0, 0, 0]
-     ]
-b = [5, 5, 5, 5]
+     [0, -1, 4, 0, 0, -1, 0, 0, 0, 0],
+     [0, 0, 0, 0, -1, 0, 4, -1, 0, 0],
+     [0, 0, 0, 0, 0, -1, -1, 4, -1, 0],
+     [0, 0, 0, 0, 0, 0, 0, -1, 4, -1],
+     [0, 0, 0, 0, 0, 0, 0, 0, -1, 4]], dtype=float)
+b = np.array([-110, -30, -40, -110, 0, -15, -90, -25, -55, -65], dtype=float)
+x0 = np.zeros_like(b, dtype=float)
 
-sol, iterations = gauss_seidel(A, b)
-print("Solução:", sol)
+solution, iterations = gauss_seidel(A, b, x0)
+
+print("Solução:", solution)
 print("Número de iterações:", iterations)
+
+
